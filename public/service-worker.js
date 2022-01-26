f578cf66462c084c6a2e3d5eeb07305b992c6bdf
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-01-26 09:40:26 -0600

    installed the service worker/files are caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,4 +9,11 @@ const FILES_TO_CACHE = [
   "./js/idb.js",
 ];
 
-
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
