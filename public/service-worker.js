b5c6a0de3398449e00dd9b5c5aaf15a94ad0c057
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-01-26 09:37:05 -0600

    added app pre/version/cache name + filestocache

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js",
+];
+
+
